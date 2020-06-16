1771cb891717d3a4d76a1f62607c5dbca57ff21d
epichfallen <dunyacengiz@gmail.com>
2020-06-17 00:14:24 +0300

    Wifi manager integrated
    wifi manager runs smoothly

--- Camber-Display.ino
+++ Camber-Display.ino
@@ -4,34 +4,51 @@
 **/
 
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
+#include <EEPROM.h>
 #include <AsyncMqttClient.h>
 #include <time.h>
 #include <GD23Z.h>
 #include "default_assets.h"
 
 
+//Variables
+int i = 0;
+int statusCode;
+const char* ssid = "text";
+const char* passphrase = "text";
+String st;
+String content;
 
 
+//Function Decalration
+bool testWifi(void);
+void launchWeb(void);
+void setupAP(void);
 
-#define WIFI_SSID "Camber"
-#define WIFI_PASSWORD "12345678"
+//Establishing Local server at port 80 whenever required
+WebServer server(80);
+
+
+//#define WIFI_SSID "Camber"
+//#define WIFI_PASSWORD "12345678"
 #define MQTT_HOST IPAddress(10, 3, 141, 1)
 #define MQTT_PORT 1883
 
 #define INPUTCOUNT 7 //COUNT OF SENSOR INPUTS
 
-const char* ntpServer = "10.3.141.1"; //ntp server address
+const char* ntpServer = "10.3.141.1";    //ntp server address
 const long  gmtOffset_sec = 3 * 60 * 60; //gmt setting
-const int   daylightOffset_sec = 0; //daylight savings?
+const int   daylightOffset_sec = 0;      //daylight savings?
 
 extern "C" {
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
 }
 
-char sensordata[INPUTCOUNT][10];//2d Char array to temporarily store sensor data
-char* config_data[5];//Char array to store config payload
+char sensordata[INPUTCOUNT][10];  //2d Char array to temporarily store sensor data
+char* config_data[5];  //Char array to store config payload
 int display_mode, i1, i2, i3, i4; //Configuration elements
 char* tags[INPUTCOUNT] = {"TWS", "TWA", "AWS", "AWA", "BS", "HEEL", "DPTH"};
 bool tws, twa, aws, awa, bs, heel, dpth = 0; // Bool for configuration; if requested by config => 1 else =>0
@@ -46,15 +63,16 @@ AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
 TimerHandle_t wifiReconnectTimer;
 
-String chipId, ownconfig, ownstate;
+String chipId, ownconfig, ownstate, ownreset;
+
 
 //================ Functions Begin ==================
 
 
-void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-}
+//void connectToWifi() {
+//  Serial.println("Connecting to Wi-Fi...");
+//  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+//}
 
 void connectToMqtt() {
   Serial.println("Connecting to MQTT...");
@@ -85,7 +103,7 @@ void onMqttConnect(bool sessionPresent) {
   mqttClient.publish(ownstate.c_str(), 0, 1, "Online", strlen("Online"));
 
   uint16_t config_packet = mqttClient.subscribe(ownconfig.c_str(), 0);
-
+  uint16_t reset_packet = mqttClient.subscribe(ownreset.c_str(), 0);
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
@@ -97,21 +115,17 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
 }
 
 void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+  Serial.println("Subscribed successfully.");
   Serial.print("  qos: ");
   Serial.println(qos);
 }
 
 void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+  Serial.println("Unsubscribed successfully.");
 }
 
 void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
+  Serial.println("Published successfully.");
   Serial.print("  packetId: ");
   Serial.println(packetId);
 }
@@ -131,14 +145,14 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     if (i1 == 0 || i2 == 0 || i3 == 0 || i4 == 0) { // check if tws was requested in configuration
       if (tws_s == 0) {
         if (uint16_t tws_packet = mqttClient.subscribe("wind/tws", 0) != 0) {
-          Serial.println("Subscribed to TWS");
+          Serial.println("Subscribing to TWS");
           tws_s = 1;
         }
       }
     }
     else if (tws_s == 1) {
       if (mqttClient.unsubscribe("wind/tws") != 0) {
-        Serial.println("Unsubscribed from TWS");
+        Serial.println("Unsubscribing from TWS");
         tws_s = 0;
       }
     }
@@ -146,14 +160,14 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     if (i1 == 1 || i2 == 1 || i3 == 1 || i4 == 1) {
       if (twa_s == 0) {
         if (uint16_t twa_packet = mqttClient.subscribe("wind/twa", 0) != 0) {
-          Serial.println("Subscribed to TWA");
+          Serial.println("Subscribing to TWA");
           twa_s = 1;
         }
       }
     }
     else if (twa_s == 1) {
       if (mqttClient.unsubscribe("wind/twa") != 0) {
-        Serial.println("Unsubscribed from TWA");
+        Serial.println("Unsubscribing from TWA");
         twa_s = 0;
       }
     }
@@ -161,14 +175,14 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     if (i1 == 2 || i2 == 2 || i3 == 2 || i4 == 2) {
       if (aws_s == 0) {
         if (uint16_t aws_packet = mqttClient.subscribe("wind/aws", 0) != 0) {
-          Serial.println("Subscribed to AWS");
+          Serial.println("Subscribing to AWS");
           aws_s = 1;
         }
       }
     }
     else if (aws_s == 1) {
       if (mqttClient.unsubscribe("wind/aws") != 0) {
-        Serial.println("Unsubscribed from AWS");
+        Serial.println("Unsubscribing from AWS");
         aws_s = 0;
       }
     }
@@ -176,14 +190,14 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     if (i1 == 3 || i2 == 3 || i3 == 3 || i4 == 3) {
       if (awa_s == 0) {
         if (uint16_t awa_packet = mqttClient.subscribe("wind/awa", 0) != 0) {
-          Serial.println("Subscribed to AWA");
+          Serial.println("Subscribing to AWA");
           awa_s = 1;
         }
       }
     }
     else if (awa_s == 1) {
       if (mqttClient.unsubscribe("wind/awa") != 0 ) {
-        Serial.println("Unsubscribed from AWA");
+        Serial.println("Unsubscribing from AWA");
         awa_s = 0;
       }
     }
@@ -191,14 +205,14 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     if (i1 == 4 || i2 == 4 || i3 == 4 || i4 == 4) {
       if (bs_s == 0) {
         if (uint16_t bs_packet = mqttClient.subscribe("boat/speed", 0) != 0) {
-          Serial.println("Subscribed to BS");
+          Serial.println("Subscribing to BS");
           bs_s = 1;
         }
       }
     }
     else if (bs_s == 1) {
       if (mqttClient.unsubscribe("boat/speed") != 0) {
-        Serial.println("Unsubscribed from BS");
+        Serial.println("Unsubscribing from BS");
         bs_s = 0;
       }
     }
@@ -206,14 +220,14 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     if (i1 == 5 || i2 == 5 || i3 == 5 || i4 == 5) {
       if (heel_s == 0) {
         if (uint16_t heel_packet = mqttClient.subscribe("boat/heel", 0) != 0) {
-          Serial.println("Subscribed to HEEL");
+          Serial.println("Subscribing to HEEL");
           heel_s = 1;
         }
       }
     }
     else if (heel_s == 1) {
       if (mqttClient.unsubscribe("boat/heel") != 0) {
-        Serial.println("Unsubscribed from HEEL");
+        Serial.println("Unsubscribing from HEEL");
         heel_s = 0;
       }
     }
@@ -221,14 +235,14 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
     if (i1 == 6 || i2 == 6 || i3 == 6 || i4 == 6) {
       if (dpth_s == 0) {
         if (uint16_t dpth_packet = mqttClient.subscribe("boat/depth", 0) != 0) {
-          Serial.println("Subscribed to DPTH");
+          Serial.println("Subscribing to DPTH");
           dpth_s = 1;
         }
       }
     }
     else if (dpth_s == 1) {
       if (mqttClient.unsubscribe("boat/depth") != 0) {
-        Serial.println("Unsubscribed from DPTH");
+        Serial.println("Unsubscribing from DPTH");
         dpth_s = 0;
       }
     }
@@ -243,6 +257,16 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
   //    starttime = atoi(starttime_str);
   //    vTaskResume(TaskHandle_1);
   //  }
+
+
+
+  if (strcmp(topic, ownreset.c_str()) == 0) { //reset wifi credentials
+    for (int i = 0; i < 96; ++i) {
+      EEPROM.write(i, 0);
+    }
+    EEPROM.commit();
+    ESP.restart();
+  }
 
   if (strcmp(topic, "wind/tws") == 0) { //get tws data
     for (int i = 0; i < len; i++) {
@@ -405,54 +429,107 @@ void printLocalTime() {
   struct tm timeinfo;
   strftime(timenow, 50, "%x - %X", &timeinfo);
   Serial.println(timenow);
-
 }
+
 
 void setup() {
   GD.begin();
   LOAD_ASSETS();
-  GD.ClearColorRGB(0x000015);
-  GD.Clear();
-  GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world");
-  GD.swap();
+  logo_debug("Loading Resources");
+  
   Serial.begin(115200);
-
+  WiFi.disconnect();
+  EEPROM.begin(512); //Initialasing EEPROM
+  delay(10);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   chipId = String((uint32_t)ESP.getEfuseMac(), HEX);
   ownconfig = String("display/" + chipId + "/config");
   ownstate = String("display/" + chipId + "/state");
+  ownreset = String("display/" + chipId + "/reset");
 
-  WiFi.onEvent(WiFiEvent);
+  //---------------------------------------- Read eeprom for ssid and pass
+  Serial.println("Reading EEPROM ssid");
 
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
-  delay(2000);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  mqttClient.setWill(ownstate.c_str(), 0, 1, "Offline", strlen("Offline"));
+  String esid;
+  for (int i = 0; i < 32; ++i)
+  {
+    esid += char(EEPROM.read(i));
+  }
+  Serial.println();
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
 
-  connectToWifi();
+  String epass = "";
+  for (int i = 32; i < 96; ++i)
+  {
+    epass += char(EEPROM.read(i));
+  }
 
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  xTaskCreate(timeEngine, "TimeEngine", 10000, NULL, 1, &TaskHandle_1);
-  
+  Serial.print("PASS: ");
+  Serial.println(epass);
+
+
+  WiFi.begin(esid.c_str(), epass.c_str());
+  if (testWifi())
+  {
+    Serial.println("Succesfully Connected!!!");
+    //  WiFi.onEvent(WiFiEvent);
+
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.onSubscribe(onMqttSubscribe);
+    mqttClient.onUnsubscribe(onMqttUnsubscribe);
+    mqttClient.onMessage(onMqttMessage);
+    mqttClient.onPublish(onMqttPublish);
+    delay(2000);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    mqttClient.setWill(ownstate.c_str(), 0, 1, "Offline", strlen("Offline"));
+
+    //  connectToWifi();
+
+    connectToMqtt();
+
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+    xTaskCreate(timeEngine, "TimeEngine", 10000, NULL, 1, &TaskHandle_1);
+    return;
+  }
+  else
+  {
+    Serial.println("Turning the HotSpot On");
+    launchWeb();
+    setupAP();// Setup HotSpot
+  }
+
+  Serial.println();
+  Serial.println("Waiting.");
+
+  while ((WiFi.status() != WL_CONNECTED))
+  {
+    Serial.print(".");
+    delay(100);
+    server.handleClient();
+  }
 }
 
 
 void loop() {
-
 }
 
+void logo_debug (char* texttoprint) {
+  GD.ClearColorRGB(0x000015);
+  GD.Clear();
+  GD.cmd_text(GD.w / 2, 228, 31, OPT_CENTER, "CAMBER LOGO");
+  GD.cmd_text(GD.w / 2, 290, 27, OPT_CENTER, texttoprint);
+  GD.swap();
+}
 
 static void timeEngine(void* pvParameters)
 {
   for (;;)
   {
-
-
     //    int now;
     //    int timeLeft;
     //    char minsecprint[6];
@@ -503,4 +580,156 @@ static void timeEngine(void* pvParameters)
     //    }
   }
   vTaskSuspend( NULL );
+}
+
+
+//----------------------------------------------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change
+bool testWifi(void)
+{
+  int c = 0;
+  Serial.println("Waiting for Wifi to connect");
+  while ( c < 20 ) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+}
+
+void launchWeb()
+{
+  Serial.println("");
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+}
+
+void setupAP(void)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == 7) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+
+    st += ")";
+    st += (WiFi.encryptionType(i) == 7) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP("Camber Display", "12345678");
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
+}
+
+void createWebServer()
+{
+  {
+    server.on("/", []() {
+
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP32 at ";
+      content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
+      content += ipStr;
+      content += "<p>";
+      content += st;
+      content += "</p><form action='/setting' method='get'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+      content += "</html>";
+      server.send(200, "text/html", content);
+    });
+    server.on("/scan", []() {
+      //setupAP();
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+      content = "<!DOCTYPE HTML>\r\n<html>go back";
+      server.send(200, "text/html", content);
+    });
+
+    server.on("/setting", []() {
+      String qsid = server.arg("ssid");
+      String qpass = server.arg("pass");
+      if (qsid.length() > 0 && qpass.length() > 0) {
+        Serial.println("clearing eeprom");
+        for (int i = 0; i < 96; ++i) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(qsid);
+        Serial.println("");
+        Serial.println(qpass);
+        Serial.println("");
+
+        Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < qsid.length(); ++i)
+        {
+          EEPROM.write(i, qsid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qsid[i]);
+        }
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < qpass.length(); ++i)
+        {
+          EEPROM.write(32 + i, qpass[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qpass[i]);
+        }
+        EEPROM.commit();
+
+        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+        statusCode = 200;
+        ESP.restart();
+      } else {
+        content = "{\"Error\":\"404 not found\"}";
+        statusCode = 404;
+        Serial.println("Sending 404");
+      }
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(statusCode, "application/json", content);
+
+    });
+  }
 }
